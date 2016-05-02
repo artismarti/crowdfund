6ddcd382e0642f649ab7d4fd6ef3a05f1bdb3891
Arti Mathanda <artim@gaz.local>
2016-05-02 22:46:52 +0100

    Numbers and Strings exercise.

---
+++ crowdfund/crowdfund.rb
@@ -0,0 +1,6 @@
+project1 = "Project ABC"
+project2 = "Project LMN"
+project3 = "Project XYZ"
+funding1 = 1000
+puts "#{project1} has $#{funding1} in funding.\n"
+puts "Projects:\n #{project1}\n #{project2}\n #{project3}\n"
